13f72b1e5907249f1cccbd24ac106d7eebe68e25
hyein89 <bihontai@gmail.com>
2025-12-10 14:58:05 +0700

    Update types.d.ts

--- app/types.d.ts
+++ app/types.d.ts
@@ -1,2 +1,2 @@
-// Untuk memberi tahu TypeScript tentang fungsi global Monitage
-declare function show_10302319(type?: string): Promise<void>;
+// Memberitahu TypeScript tentang fungsi global Monitage
+declare function show_10302319(): Promise<void>;
